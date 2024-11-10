8d8c13f706b457a33f98839c2a2e2f86915e2aa1
Veljko Fajnisevic <keychiper.cy@gmail.com>
2024-11-10 19:19:36 +0100

    Empty Project Starting from a scratch...

--- src/main/java/com/project/game/appuser/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package com.project.game.appuser;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
--- src/main/java/com/project/game/email/EmailSender.java
+++
@@ -1,6 +0,0 @@
-package com.project.game.email;
-
-public interface EmailSender {
-
-    void send(String to, String email);
-}
